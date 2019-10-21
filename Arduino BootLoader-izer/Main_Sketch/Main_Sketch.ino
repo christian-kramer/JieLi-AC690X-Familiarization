7f8f717870ccecf80d0768b2bdfe505805f037ab
christian <christian.kramer@mail.com>
2019-10-21 11:07:29 -0500

    Working arduino sketch to put AC6905 into UBOOT

---
+++ Arduino BootLoader-izer/Main_Sketch/Main_Sketch.ino
@@ -0,0 +1,47 @@
+#define DATA_PIN 8
+#define CLOCK_PIN 7
+
+void clockOut(int value) {
+  digitalWrite(DATA_PIN, value);
+  digitalWrite(CLOCK_PIN, 0);
+  delayMicroseconds(7);
+  digitalWrite(CLOCK_PIN, 1);
+  delayMicroseconds(3);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  clockOut(0);
+  clockOut(0);
+  clockOut(0);
+  
+  clockOut(1);
+  
+  clockOut(0);
+  
+  clockOut(1);
+  clockOut(1);
+  
+  clockOut(0);
+  
+  clockOut(1);
+  clockOut(1);
+  clockOut(1);
+  
+  clockOut(0);
+  
+  clockOut(1);
+  clockOut(1);
+  clockOut(1);
+  clockOut(1);
+
+  delayMicroseconds(2);
+  digitalWrite(DATA_PIN, 0);
+  digitalWrite(CLOCK_PIN, 0);
+}
